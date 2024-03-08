c135b2e88581c4d628ac06c7698ce4f1181eec73
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-03-08 10:10:51 -0500

    Checking gh actions works to catch tf code mistake

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,9 +19,9 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storageacct" {
-  name                     = "storageaccountjoe"
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
+  name = "storageaccountjoe"
+  #   resource_group_name      = azurerm_resource_group.resource_group.name
+  #   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
